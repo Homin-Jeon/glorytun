bb3d7591f2ab7ceae4508cede0dc43d72b187232
ethan@beyless.com <ethan@beyless.com>
2024-05-10 09:38:13 +0900

    Add bench test of aes-gcm

--- src/bench.c
+++ src/bench.c
@@ -10,15 +10,65 @@
 #include "../argz/argz.h"
 #include "../mud/aegis256/aegis256.h"
 
-#define NPUBBYTES 32
-#define KEYBYTES  32
-#define ABYTES    16
+typedef struct cipher_interface{
+    char name[20];
+
+    size_t ABYTES;
+    size_t NPUBBYTES;
+    size_t KEYBYTES;
+
+    int (*is_available)(void);
+    void (*keygen)(unsigned char key[]);
+    int (*encrypt)(unsigned char *c,
+                    unsigned long long *clen_p,
+                    const unsigned char *m,
+                    unsigned long long mlen,
+                    const unsigned char *ad,
+                    unsigned long long adlen,
+                    const unsigned char *nsec,
+                    const unsigned char *npub,
+                    const unsigned char *k);
+} cipher_interface_t;
+
+
+static cipher_interface_t aes_if = {
+    "aes-gcm",
+    crypto_aead_aes256gcm_ABYTES,
+    crypto_aead_aes256gcm_NPUBBYTES,
+    crypto_aead_aes256gcm_KEYBYTES,
+
+    crypto_aead_aes256gcm_is_available,
+    crypto_aead_aes256gcm_keygen,
+    crypto_aead_aes256gcm_encrypt
+};
+
+static cipher_interface_t aegis_if = {
+    "aegis256",
+    crypto_aead_aegis256_ABYTES,
+    crypto_aead_aegis256_NPUBBYTES,
+    crypto_aead_aegis256_KEYBYTES,
+
+    aegis256_is_available,
+    crypto_aead_aegis256_keygen,
+    crypto_aead_aegis256_encrypt
+};
+
+static cipher_interface_t chacha_if = {
+    "chacha20poly1305",
+    crypto_aead_chacha20poly1305_ABYTES,
+    crypto_aead_chacha20poly1305_NPUBBYTES,
+    crypto_aead_chacha20poly1305_KEYBYTES,
+
+    NULL,
+    crypto_aead_chacha20poly1305_keygen,
+    crypto_aead_chacha20poly1305_encrypt
+};
 
 int
 gt_bench(int argc, char **argv)
 {
     struct argz bench_argz[] = {
-        {"aes|chacha", NULL, NULL, argz_option},
+        {"aes|chacha|aegis", NULL, NULL, argz_option},
         {NULL}};
 
     if (argz(bench_argz, argc, argv))
@@ -29,28 +79,53 @@ gt_bench(int argc, char **argv)
         return 1;
     }
 
-    int term = isatty(1);
-    int aes = argz_is_set(bench_argz, "aes");
-    int chacha = argz_is_set(bench_argz, "chacha");
+    cipher_interface_t *cipher_if;
 
-    if (!aegis256_is_available()) {
-        if (aes) {
-            gt_log("aes is not available on your platform\n");
-            return 1;
-        }
-        chacha = 1;
+    int term = isatty(1);
+    
+    cipher_if = argz_is_set(bench_argz, "aes")?&aes_if:
+                argz_is_set(bench_argz, "aegis")?&aegis_if:&chacha_if;
+
+    if (cipher_if == NULL) {
+        gt_log("cipher_if is NULL\n");
+        return 1;
     }
 
-    unsigned char buf[1450 + ABYTES];
-    unsigned char npub[NPUBBYTES];
-    unsigned char key[KEYBYTES];
+    if (cipher_if->is_available != NULL) {
+        if (!cipher_if->is_available()) {
+            gt_log("%s is not available on your platform\n", cipher_if->name);
+            return 1;
+        }
+    }
+    else if (cipher_if != &chacha_if) {
+        gt_log("%s's cipher_if->is_available is NULL.\n", cipher_if->name);
+        cipher_if = &chacha_if;
+    }
 
+    if (cipher_if->encrypt == NULL) {
+        gt_log("%s's cipher_if->encrypt is NULL\n", cipher_if->name);
+        return 1;
+    }
+
+    unsigned char buf[1450 + cipher_if->ABYTES];
+    unsigned char nonce[cipher_if->NPUBBYTES];
+    unsigned char key[cipher_if->KEYBYTES];
+
+    unsigned long long ciphertext_len;
+    
+    if(cipher_if->keygen != NULL) {
+        cipher_if->keygen(key);
+    }
+    else {
+        gt_log("%s's cipher_if->keygen is null\n", cipher_if->name);
+        return 1;
+    }
+
+    randombytes_buf(nonce, sizeof nonce); 
     memset(buf, 0, sizeof(buf));
-    randombytes_buf(npub, sizeof(npub));
-    randombytes_buf(key, sizeof(key));
 
     if (term) {
-        printf("cipher: %s\n\n", GT_CIPHER(chacha));
+        printf("\ncipher: %s\n\n", cipher_if->name);
         printf("  size       min           mean            max      \n");
         printf("----------------------------------------------------\n");
     }
@@ -69,12 +144,9 @@ gt_bench(int argc, char **argv)
             int64_t base = (int64_t)clock();
 
             while (!gt_quit && bytes <= bytes_max) {
-                if (chacha) {
-                    crypto_aead_chacha20poly1305_encrypt(
-                            buf, NULL, buf, size, NULL, 0, NULL, npub, key);
-                } else {
-                    aegis256_encrypt(buf, NULL, buf, size, NULL, 0, npub, key);
-                }
+                cipher_if->encrypt(
+                        buf, &ciphertext_len, buf, size, NULL, 0, NULL, nonce, key);
+
                 bytes += size;
             }
 
@@ -108,7 +180,7 @@ gt_bench(int argc, char **argv)
             printf("\n");
         } else {
             printf("bench %s %"PRIi64" %"PRIi64" %"PRIi64" %"PRIi64"\n",
-                    GT_CIPHER(chacha), size, mbps.min, mbps.mean, mbps.max);
+                    cipher_if->name, size, mbps.min, mbps.mean, mbps.max);
         }
 
         size += 2 * 5 * 13;
